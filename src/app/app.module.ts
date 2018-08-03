bf40b97ffa73d9b4c2da3be141d50717ee2dca36
Divij <divij.shrivastava@gmail.com>
2018-08-03 08:31:11 +0530

    Building recipe app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipeSummaryComponent } from './recipe-summary/recipe-summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeSummaryComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipe-summary/recipe-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-summary',
+  templateUrl: './recipe-summary.component.html',
+  styleUrls: ['./recipe-summary.component.css']
+})
+export class RecipeSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
