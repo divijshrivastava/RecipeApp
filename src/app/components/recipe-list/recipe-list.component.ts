237a3bb576f04400e29ce2e6b7ea3df01c6a14a6
Divij <divij.shrivastava@gmail.com>
2018-08-04 11:08:14 +0530

    Using structural directives on components. Commit 4.1 and 4.2

--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -8,17 +8,17 @@ import { Recipe } from '../../model/recipe';
 })
 export class RecipeListComponent {
 
-   recipes: Recipe[];
+  recipes: Recipe[];
   constructor() {
-   this.recipes = [
-     (new Recipe('Dosa',
-      'South Indian Roti: It\'s very delicious',
-       null, null, null)),
-      new Recipe('Pav Bhaji', 'Punjabi Dish: It\'s very very tasty', 
-    null, null, null),
-    (new Recipe('Kaju Kari', 'Consists of cashewnuts and gravy', null, null, null))
-      ]
-   ;
+    this.recipes = [
+      (new Recipe('Dosa',
+        'South Indian Roti: It\'s very delicious',
+        null, null, null)),
+      new Recipe('Pav Bhaji', 'Punjabi Dish: It\'s very very tasty',
+        null, null, null),
+      (new Recipe('Kaju Kari', 'Consists of cashewnuts and gravy', null, null, null))
+    ]
+      ;
   }
 
 
